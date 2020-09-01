c3337e893bd25571adb245b88af474ea5cf519e4
Luis <lvegerano422@gmail.com>
2020-09-01 16:15:20 -0500

    [CHORE] Octanify

--- addon/components/baremetrics-calendar.js
+++ addon/components/baremetrics-calendar.js
@@ -1,36 +1,27 @@
-/* global Calendar */
-
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import JQuery from 'jquery';
 import { equal } from '@ember/object/computed';
-import { run } from '@ember/runloop';
+import { bind, run } from '@ember/runloop';
+import Calendar from 'BaremetricsCalendar';
 
 /**
  * @module
- * @augments ember/Component
+ * @augments @glimmer/Component
  */
-export default Component.extend({
-
-  // -------------------------------------------------------------------------
-  // Dependencies
-
-  // -------------------------------------------------------------------------
-  // Attributes
-
-  classNames: [
-    'baremetrics-calendar',
-    'daterange'
-  ],
-
+export default class BaremetricsCalendarComponent extends Component {
   // -------------------------------------------------------------------------
   // Events
 
-  didRender() {
-    this.set('calendar', this._buildCalendar());
-  },
+  // didRender() {
+  //   this.set('calendar', this._buildCalendar());
+  // },
 
   // -------------------------------------------------------------------------
   // Properties
+
+  callback = bind(this, this._parseCallback);
 
   /**
    * When used as a single date picker, `currentDate` should be the value of the
@@ -38,7 +29,7 @@ export default Component.extend({
    *
    * @type {Date}
    */
-  currentDate: null,
+  currentDate = null;
 
   /**
    * When used as a double date picker (i.e. date ranges), `startDate` should be
@@ -49,7 +40,7 @@ export default Component.extend({
    *
    * @type {Date}
    */
-  startDate: undefined,
+  @tracked startDate = undefined;
 
   /**
    * When used as a double date picker (i.e. date ranges), `endDate` should be
@@ -57,7 +48,7 @@ export default Component.extend({
    *
    * @type {Date}
    */
-  endDate: null,
+  endDate = null;
 
   /**
    * When used as a double date picker (i.e. date ranges), `earliestDate`
@@ -65,7 +56,7 @@ export default Component.extend({
    *
    * @type {Date}
    */
-  earliestDate: null,
+  earliestDate = null;
 
   /**
    * When used as a double date picker (i.e. date ranges), `latestDate`
@@ -73,14 +64,14 @@ export default Component.extend({
    *
    * @type {Date}
    */
-  latestDate: null,
+  latestDate = null;
 
   /**
    * A moment.js format string for how dates should appear in the inputs.
    *
    * @type {String}
    */
-  inputFormat: 'MMMM D, YYYY',
+  inputFormat = 'MMMM D, YYYY';
 
   /**
    * A moment.js format string for how month labels should appear when switching
@@ -88,7 +79,7 @@ export default Component.extend({
    *
    * @type {String}
    */
-  jumpMonthFormat: 'MMMM',
+  jumpMonthFormat = 'MMMM';
 
   /**
    * A moment.js format string for how year labels should appear when switching
@@ -96,7 +87,7 @@ export default Component.extend({
    *
    * @type {String}
    */
-  jumpYearFormat: 'YYYY',
+  jumpYearFormat = 'YYYY';
 
   /**
    * A moment.js format string for how preset labels should appear in the preset
@@ -104,14 +95,14 @@ export default Component.extend({
    *
    * @type {String}
    */
-  presetFormat: null,
+  presetFormat = null;
 
   /**
    * An array of strings for the day of week labels, starting with Sunday.
    *
    * @type {String[]}
    */
-  dayLabels: [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa' ],
+  dayLabels = [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa' ];
 
   /**
    * An array of preset objects to display. If not supplied, it will default to a basic
@@ -127,7 +118,7 @@ export default Component.extend({
    *
    * @type {Object[]}
    */
-  presets: false,
+  presets = false;
 
   /**
    * When using in single date mode (see `.type`), use this flag to indicate
@@ -135,7 +126,7 @@ export default Component.extend({
    *
    * @type {Boolean}
    */
-  required: false,
+  required = false;
 
   /**
    * Placeholder string that appears in single date mode (see `.type`), and only
@@ -143,14 +134,14 @@ export default Component.extend({
    *
    * @type {String}
    */
-  placeholder: null,
+  placeholder = null;
 
   /**
    * In double mode (see `.type`), is a single day a valid range selection?
    *
    * @type {Boolean}
    */
-  sameDayRange: true,
+  sameDayRange = true;
 
   /**
    * What kind of date picker is this:
@@ -164,26 +155,26 @@ export default Component.extend({
    *
    * @type {String}
    */
-  type: computed('startDate', function() {
-    if (typeof this.get('startDate') !== 'undefined') {
+  get type() {
+    if (typeof this.startDate !== 'undefined') {
       return 'double';
     }
     return 'single';
-  }),
+  }
 
   /**
    * Convenience computed property for the `type === 'single'`
    *
    * @type {Boolean}
    */
-  isSingle: equal('type', 'single'),
+  @equal('type', 'single') isSingle;
 
   /**
    * Convenience computed property for the `type === 'double'`
    *
    * @type {Boolean}
    */
-  isDouble: equal('type', 'double'),
+  @equal('type', 'double') isDouble;
 
   // -------------------------------------------------------------------------
   // Methods
@@ -191,43 +182,41 @@ export default Component.extend({
   /**
    * Builds the config object that the Calendar constructor expects
    *
-   * @method _buildCalendar
+   * @method setupCalendar
    * @private
    * @return {Object}
    */
-  _buildCalendar() {
-    let component = this;
-    let element = this.$('<div class="baremetrics-calendar">');
-    this.$().empty().append(element);
+  @action
+  setupCalendar(element) {
+    // let component = this;
     let config = {
-      element: element,
+      element: JQuery(element),
       format: {
-        input: this.get('inputFormat'),
-        jump_month: this.get('jumpMonthFormat'),
-        jump_year: this.get('jumpYearFormat')
+        input: this.inputFormat,
+        jump_month: this.jumpMonthFormat,
+        jump_year: this.jumpYearFormat
       },
-      days_array: this.get('dayLabels'),
-      presets: this.get('presets'),
-      placeholder: this.get('placeholder'),
-      callback() {
-        component._parseCallback(this);
-      }
+      days_array: this.dayLabels,
+      presets: this.presets,
+      placeholder: this.placeholder,
+      callback: this.callback,
     };
-    if (this.get('type') === 'single') {
-      config.current_date = this.get('currentDate');
-      config.required = this.get('required');
-      element.addClass('daterange--single');
+
+    if (this.type === 'single') {
+      config.current_date = this.currentDate;
+      config.required = this.required;
+      element.classList.add('daterange--single');
     } else {
-      config.earliest_date = this.get('earliestDate');
-      config.latest_date = this.get('latestDate');
-      config.start_date = this.get('startDate');
-      config.end_date = this.get('endDate');
-      config.format.preset = this.get('presetFormat');
-      config.same_day_range = this.get('sameDayRange');
-      element.addClass('daterange--double');
+      config.earliest_date = this.earliestDate;
+      config.latest_date = this.latestDate;
+      config.start_date = this.startDate;
+      config.end_date = this.endDate;
+      config.format.preset = this.presetFormat;
+      config.same_day_range = this.sameDayRange;
+      element.classList.add('daterange--double');
     }
     return new Calendar(config);
-  },
+  }
 
   /**
    * Invoked by the Calendar library when a date value changes. Responsible for
@@ -241,8 +230,8 @@ export default Component.extend({
   _parseCallback({ current_date, start_date, end_date }) {
     run(() => {
       let result = start_date ? { startDate: start_date, endDate: end_date } : current_date;
-      this.sendAction('onchange', result);
+      // this.sendAction('onchange', result);
+      this.args.onchange?.(result)
     });
   }
-
-});
+}
--- index.js
+++ index.js
@@ -1,58 +1,24 @@
 /* eslint-env node */
 'use strict';
 
-const path = require('path');
-const Funnel = require('broccoli-funnel');
-const MergeTrees = require('broccoli-merge-trees');
-const map = require('broccoli-stew').map;
-
 module.exports = {
-  name: 'ember-baremetrics-calendar',
+  name: require('./package').name,
 
-  init: function () {
-    if (this._super.init) {
-      this._super.init.apply(this, arguments);
-    }
+  options: {
+    autoImport: {
+      alias: {
+        'BaremetricsCalendar': 'BaremetricsCalendar/public/js/Calendar.js'
+      }
+    },
   },
 
-  included: function () {
+  included(appOrAddon) {
     this._super.included.apply(this, arguments);
-
-    let findHost = this._findHost;
-    let app = findHost.call(this);
-    this.app = app;
-
-    let options = app.options.baremetricsCalendar || {};
-
-    app.import('vendor/moment.js');
-    app.import('vendor/Calendar.js');
+    const app = appOrAddon.app || appOrAddon;
+    const options = app.options.baremetricsCalendar || {};
 
     if (options.includeStyles !== false) {
-      app.import('vendor/ember-baremetrics-calendar/application.css');
+      app.import('node_modules/BaremetricsCalendar/public/css/application.css');
     }
-  },
-
-  treeForVendor: function (vendorTree) {
-    let BareMetricsTree = new Funnel(path.join(this.project.root, 'node_modules', 'BaremetricsCalendar', 'public', 'js'));
-    //Only add if not FastBoot
-    BareMetricsTree = map(BareMetricsTree, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
-
-    let MomentTree = new Funnel(path.join(this.project.root, 'node_modules', 'moment'), {
-      files: ['moment.js'],
-    });
-    // Only add if not FastBoot
-    MomentTree = map(MomentTree, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
-
-    let styleTree = new Funnel(path.join(this.project.root, 'node_modules', 'BaremetricsCalendar', 'public', 'css'), {
-      destDir: 'ember-baremetrics-calendar'
-    });
-
-    let trees = [BareMetricsTree, MomentTree, styleTree];
-    // Check if vendor tree is null
-    if (vendorTree) {
-      trees.push(vendorTree);
-    }
-
-    return new MergeTrees(trees);
   },
 };
